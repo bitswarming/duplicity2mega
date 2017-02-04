3beb6da204ab5d1e939d57d5f3636e03d09b3e3a
bit swarm <bitswarming@gmail.com>
2017-02-04 23:57:52 +0200

    removed comments

--- configs/backup.sh
+++ configs/backup.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#export PASSPHRASE
-#export GNUPGHOME="/home/porky/.gnupg"
-#HOME="/opt/backup/mega"
-#export HOME=$HOME
-# Load our credentials
-#source "$HOME/.credentials.conf"
-#export PASSPHRASE
-#export VOLSIZE
-#GPG_KEY='6FBCFBDC'
-
 duplicity \
     --verbosity notice \
     --full-if-older-than $OLDERTHAN \
@@ -20,8 +10,3 @@ duplicity \
     --exclude '**' \
     /hostdisk \
     file:///media/backups/$(hostname) -v info
-
-#unset PASSPHRASE
-# sudo docker exec -it megafuse bash /root/backup.sh
-#    --encrypt-key="$GPG_KEY" \
-#    --sign-key="$GPG_KEY" \
